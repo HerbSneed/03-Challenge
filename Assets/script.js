4f981252422de2c91f00f495c4e3fc0c4e212000
Herb Sneed <hmsneed@gmail.com>
2023-04-03 18:14:23 -0400

    Java Script Work Day 01

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,61 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-var userlength =  prompt("What is the length of your desired password?");
-var userLowerCaseLet = prompt("Would you like lowercase letters?");
-var lowerCaseLetNum = prompt("How many?");
-var userUpperCaseLet = prompt("Would you like uppercase letters?");
-var upperCaseLetNum = prompt("How many?");
-var userNum = prompt("Would you like numbers?");
-var userNumNum = prompt("How many?");
-var userSpecialChar = prompt("Would you like special characters?");
-var userSpecialCharNum = prompt("How many?");
-const lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
 const upperAlpha = lowerAlpha.map(lowerAlpha => lowerAlpha.toUpperCase());
 const numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8","9"];
 
+
+function generatePassword() {
+var userlength =  prompt("What is the length of your desired password?");
+
 if (userlength < 8) {
-  console.log("Please enter a number between 8 and 128.");
+  alert("Enter a length that is at least 8 characters.");
 }
-}
+else if (userlength > 7) {
+  var userLowerCaseLet = prompt("Would you like lowercase letters?");
+    if (userLowerCaseLet === "yes") {
+      var lowerCaseLetNum = prompt("How many?");
+      var randomlowercaselet = "";
+      lowerAlphaLength = lowerAlpha.length;
+      for (var i = 0; i < lowerCaseLetNum; i++) {
+        randomlowercaselet += lowerAlpha.charAt(Math.floor(Math.random() * lowerAlphaLength));
+        }
+        console.log(randomlowercaselet);
+      }
+    }  
+  }
+
+
+
+
+
+// var userUpperCaseLet = prompt("Would you like uppercase letters?");
+// var upperCaseLetNum = prompt("How many?");
+// var userNum = prompt("Would you like numbers?");
+// var userNumNum = prompt("How many?");
+// var userSpecialChar = prompt("Would you like special characters?");
+// var userSpecialCharNum = prompt("How many?");
+
+
+
+
+
+// }
+
 
 
 
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
