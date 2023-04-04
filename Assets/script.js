7571235177316c8f9a1252eb52a0c95f063ded84
Herb Sneed <hmsneed@gmail.com>
2023-04-04 14:46:01 -0400

    4 April 23 break

--- Assets/script.js
+++ Assets/script.js
@@ -2,48 +2,93 @@
 // var generateBtn = document.querySelector("#generate");
 
 const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
-const upperAlpha = lowerAlpha.map(lowerAlpha => lowerAlpha.toUpperCase());
-const numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8","9"];
-
+const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 function generatePassword() {
-var userlength =  prompt("What is the length of your desired password?");
-
-if (userlength < 8) {
-  alert("Enter a length that is at least 8 characters.");
+var orderedpassword = "";
+var userLength =  prompt("What is the length of your desired password?");
+if (userLength < 8) {
+  alert("Enter a length that is at least 8 characters. Generate password again.");
 }
-else if (userlength > 7) {
+else if (userLength > 7) {
   var userLowerCaseLet = prompt("Would you like lowercase letters?");
-    if (userLowerCaseLet === "yes") {
-      var lowerCaseLetNum = prompt("How many?");
-      var randomlowercaselet = "";
-      lowerAlphaLength = lowerAlpha.length;
-      for (var i = 0; i < lowerCaseLetNum; i++) {
-        randomlowercaselet += lowerAlpha.charAt(Math.floor(Math.random() * lowerAlphaLength));
-        }
-        console.log(randomlowercaselet);
+  if (userLowerCaseLet === "yes") {
+    var lowerCaseLetNum = prompt("How many?");
+    var randomLowerCaseLet = "";
+    var remaining = userLength - lowerCaseLetNum;
+    lowerAlphaLength = lowerAlpha.length;
+    for (var i = 0; i < lowerCaseLetNum; i++) {
+      randomLowerCaseLet += lowerAlpha.charAt(Math.floor(Math.random() * lowerAlphaLength));
+    }
+    console.log(randomLowerCaseLet);
+    orderedpassword = randomLowerCaseLet;
+    
+    if (remaining > 0) {
+      alert("You have " + (remaining) + " remaining.")
+    }
+    else {
+      return randomLowerCaseLet;
+    }
+  } else if (userLowerCaseLet === yes || no) {
+    var userUpperCaseLet = prompt("Would you like uppercase letters?");
+    if (userUpperCaseLet === "yes") {
+      var upperCaseLetNum = prompt("How many?");
+      var randomUpperCaseLet = "";
+      remaining = remaining - upperCaseLetNum;
+      upperAlphaLength = upperAlpha.length;
+      for (var i = 0; i < upperCaseLetNum; i++) {
+        randomUpperCaseLet += upperAlpha.charAt(Math.floor(Math.random() * upperAlphaLength));
       }
-    }  
+      console.log(randomLowerCaseLet + randomUpperCaseLet);
+      if (remaining > 0) {
+        alert("You have " + (remaining) + " remaining.")
+      }
+      else {
+        return randomLowerCaseLet + randomUpperCaseLet;
+      }
+    } 
+  }
+  var userNum = prompt("Would you like numbers?");
+  if (userNum === "yes") {
+    var userNumChoice = prompt("How many?");
+    var randomNum = "";
+    remaining = remaining - userNumChoice;
+    numLength = numbers.length;
+    for (var i = 0; i < userNumChoice; i++) {
+      randomNum += numbers.charAt(Math.floor(Math.random() * numLength));
+    }
+    console.log(randomLowerCaseLet + randomUpperCaseLet + randomNum);
+    if (remaining > 0) {
+      alert("You have " + (remaining) + " remaining.")
+    }
+    else {
+      return randomLowerCaseLet + randomUpperCaseLet + randomNum;
+    }
   }
 
-
-
-
-
-// var userUpperCaseLet = prompt("Would you like uppercase letters?");
-// var upperCaseLetNum = prompt("How many?");
-// var userNum = prompt("Would you like numbers?");
-// var userNumNum = prompt("How many?");
-// var userSpecialChar = prompt("Would you like special characters?");
-// var userSpecialCharNum = prompt("How many?");
-
-
-
-
-
-// }
-
-
+  var userSpecialChar = prompt("Would you like special characters?");
+  if (userSpecialChar === "yes") {
+    var userSpeCharChoice = prompt("How many?");
+    var randomSpecChar = "";
+    remaining = remaining - userSpeCharChoice;
+    specialCharLength = specialChar.length;
+    for (var i = 0; i < userSpeCharChoice; i++) {
+      randomSpecChar += numbers.charAt(Math.floor(Math.random() * specialCharLength));
+    }
+    console.log(randomLowerCaseLet + randomUpperCaseLet + randomNum + randomSpecChar);
+    if (remaining > 0) {
+      alert("You have " + (remaining) + " remaining.")
+    }
+    else {
+      return randomLowerCaseLet + randomUpperCaseLet + randomNum + randomSpecChar;
+    }
+  }
+  
+  
+}
+}
 
 
 
